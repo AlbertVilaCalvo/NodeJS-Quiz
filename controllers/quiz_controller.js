6bc19f75df0614fa7fd4adfe09ecc4dc822321ad
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2015-06-21 18:43:30 +0200

    Depliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,14 +2,14 @@ var models = require('../models/models.js');
 
 // GET /quizzes/question
 exports.question = function(req, res) {
-  models.Quiz.findAll().success(function(quiz) {
+  models.Quiz.findAll().then(function(quiz) {
     res.render('quizes/question', {pregunta : quiz[0].pregunta});
   });
 };
 
 // GET /quizzes/answer?respuesta=Roma
 exports.answer = function(req, res) {
-  models.Quiz.findAll().success(function(quiz) {
+  models.Quiz.findAll().then(function(quiz) {
     if (req.query.respuesta === quiz[0].respuesta) {
       res.render('quizes/answer', {respuesta : 'Correcto'});
     } else {
