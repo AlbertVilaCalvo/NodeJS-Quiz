180420cbfe257dac2644372745a159a19258804e
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2015-06-22 20:47:15 +0200

    Autenticacion de usuarios

---
+++ controllers/session_controller.js
@@ -0,0 +1,31 @@
+// GET /login - formulario login
+exports.new = function(req, res) {
+  var errors = req.session.errors || {};
+  req.session.errors = {};
+  res.render('sessions/new', {errors: errors});
+};
+
+// POST /login - crear session
+exports.create = function(req, res) {
+  var login =    req.body.login;
+  var password = req.body.password;
+
+  var userController = require('./user_controller');
+  userController.autenticar(login, password, function(error, user) {
+    if (error) {
+      req.session.errors = [{'message': 'Se ha producido un error: ' + error}];
+      res.redirect('/login');
+      return;
+    }
+    // Crear req.session.user y guardar campos id y username
+    req.session.user = { id: user.id, username: user.username };
+    // Redireccion a path anterior a login (ver app.js)
+    res.redirect(req.session.redir.toString());
+  });
+};
+
+// POST /logout - destruir session
+exports.destroy = function(req, res) {
+  delete req.session.user;
+  res.redirect(req.session.redir.toString()); // redirect path anterior al login (ver app.js)
+};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -16,6 +17,10 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // debería ser destroy
 
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
