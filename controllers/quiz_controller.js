623563a141b67f095c5c56212435ca4597f5bf44
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2015-06-22 12:31:27 +0200

    Validación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -20,18 +20,18 @@ exports.index = function(req, res) {
     var search = '%' + req.query.search.split(' ').join('%') + '%';
     // console.log(search);
     models.Quiz.findAll({where: ["pregunta like ?", search], order: 'pregunta ASC'}).then(function(quizes) {
-      res.render('quizes/index', {quizes : quizes});
+      res.render('quizes/index', {quizes : quizes, errors: []});
     }).catch(function(error) { next(error); });
   } else {
     models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index', {quizes : quizes});
+      res.render('quizes/index', {quizes : quizes, errors: []});
     }).catch(function(error) { next(error); });
   }
 };
 
 // GET /quizzes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', {quiz : req.quiz});
+  res.render('quizes/show', {quiz : req.quiz, errors: []});
 };
 
 // GET /quizzes/:id/answer?respuesta=Roma
@@ -40,7 +40,7 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz : req.quiz, respuesta : resultado});
+  res.render('quizes/answer', {quiz : req.quiz, respuesta : resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -48,7 +48,7 @@ exports.new = function(req, res) {
   var quiz = models.Quiz.build(
     {pregunta: 'Pregunta', respuesta: 'Respuesta'} // valores iniciales formulario
   );
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
@@ -56,7 +56,17 @@ exports.create = function(req, res) {
   console.log(req.body.quiz);
   var quiz = models.Quiz.build( req.body.quiz );
   // Guardar en DB
-  quiz.save({fields: ['pregunta', 'respuesta']}).then(function() {
-    res.redirect('/quizes');
+  quiz
+  .validate()
+  .then(function(err){
+    if (err) {
+      res.render('quizes/new', {quiz: quiz, errors: err.errors});
+    } else {
+      quiz
+      .save({fields: ['pregunta', 'respuesta']})
+      .then(function() {
+        res.redirect('/quizes');
+      });
+    }
   });
 };
